bc47fb1b0926f368be78d718353ee5422caa91b3
Alexander BORISOV <67188470+PoweredByRedBull@users.noreply.github.com>
2020-09-30 16:11:55 +0900

    Create Cards.js

---
+++ components/Cards.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem src='img/block.png' label='Game' path='/services' />
+            <CardItem path='/services' />
+            <CardItem path='/services' />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
