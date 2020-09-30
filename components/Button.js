a8534dbd7c3ec521d3c362e2362e8872439fd8da
Alexander BORISOV <67188470+PoweredByRedBull@users.noreply.github.com>
2020-09-30 16:14:24 +0900

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -2,29 +2,17 @@ import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
 
-const BUTTON_STYLES = ["btn-primary", "btn-outline"];
-const BUTTON_SIZES = ["btn-medium", "btn-large"];
+const BUTTON_STYLES = ["btn-primary", "btn-secondary"];
 
-export const Button = ({
-  children,
-  type,
-  onClick,
-  buttonStyle,
-  buttonSize,
-  to,
-}) => {
+export const Button = ({ children, type, onClick, buttonStyle, to }) => {
   const checkButtonStyle = BUTTON_STYLES.includes(buttonStyle)
     ? buttonStyle
     : BUTTON_STYLES[0];
 
-  const checkButtonSize = BUTTON_SIZES.includes(buttonSize)
-    ? buttonSize
-    : BUTTON_SIZES[0];
-
   return (
-    <Link to={to} className='btn'>
+    <Link to={to}>
       <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        className={`btn ${checkButtonStyle}`}
         onClick={onClick}
         type={type}
       >
